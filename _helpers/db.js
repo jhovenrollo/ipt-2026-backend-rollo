6ff0035e358aa3cbc09b2cfd6a4f8bf1b5ae9ce6
Jhun Ardy Cabrera <jhovencabrera@gmail.com>
2026-05-19 21:35:47 +0800

    fix database connection using DATABASE_URL

--- _helpers/db.js
+++ _helpers/db.js
@@ -1,24 +1,18 @@
 const { Sequelize } = require('sequelize');
-require('dotenv').config();
 
-const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT || 3306,
-        dialect: 'mysql',
-        logging: false
-    }
-);
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'mysql',
+    dialectOptions: {
+        ssl: false
+    },
+    logging: false
+});
 
 const db = {};
 db.sequelize = sequelize;
 db.Account = require('../accounts/account.model')(sequelize);
 db.RefreshToken = require('../accounts/refresh-token.model')(sequelize);
 
-// Relations
 db.Account.hasMany(db.RefreshToken, { onDelete: 'CASCADE' });
 db.RefreshToken.belongsTo(db.Account);
 
